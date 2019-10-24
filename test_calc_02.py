58ae836eaddb62f324831291bb892c22f28082d2
Chiien <fuchienhsu12@gmail.com>
2019-10-24 20:52:26 -0300

    Fix(test_calc_02.py) change assert value

--- test_calc_02.py
+++ test_calc_02.py
@@ -2,4 +2,4 @@ from class_02 import Calculate_IRRF
 
 def test_class_02_desconto_IRRF():
     calcIRRF = Calculate_IRRF(3000)
-    assert calcIRRF.calculate() == 2612.55
+    assert calcIRRF.calculate() == 57.45
