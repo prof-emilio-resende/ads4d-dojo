2f4dd8f691201db820776b18081682999a5dbac8
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2019-10-24 21:27:37 -0300

    Add files via upload
    Extendendo classe, corrigindo teste.

---
+++ class_02.py
@@ -0,0 +1,16 @@
+class Calculate_IRRF:
+    def __init__(self, salarioBase):
+        self.salarioBase = salarioBase
+    
+    def calculate(self):
+        salary = self.salarioBase - (self.salarioBase * 0.11)
+        if (salary > 1903.99 and salary < 2826.65):
+            discount_INSS = self.salarioBase * 0.11
+            discount_IRRF = (self.salarioBase - discount_INSS - 1903.99) * 0.075
+            return round(discount_IRRF, 2)
+        return self.salarioBase
+    
+    def aplicar_desconto(self):
+        if self.salarioBase >= 3751.06 and self.salarioBase <= 4464.68:
+            return round((self.salarioBase - 1903.98) * 0.225, 2)
+        return self.salarioBase
--- test_aliquota_22.py
+++ test_aliquota_22.py
@@ -1,3 +1,6 @@
-def test_aliquota_22_50()
-    aliquota = Aliquota(4000.0)
+from class_02 import Calculate_IRRF
+import pytest
+
+def test_aliquota_22_50():
+    aliquota = Calculate_IRRF(4000.0)
     assert aliquota.aplicar_desconto() == 471.60
